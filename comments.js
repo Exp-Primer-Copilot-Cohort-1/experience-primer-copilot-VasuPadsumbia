77daa00807c5592626cccc74e7eeb9f8af4dca23
VasuPadsumbia <vasu.padsumbia@gmail.com>
2023-12-23 11:53:07 +0100

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    // Handle incoming requests here
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
